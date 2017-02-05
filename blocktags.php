c245188ae6c7fcafb4ce0ed8c4c5b889054a4096
Michael Dekker <git@michaeldekker.nl>
2017-02-05 07:54:05 +0100

    Update blocktags.php

--- blocktags.php
+++ blocktags.php
@@ -24,7 +24,7 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-if (!defined('_PS_VERSION_'))
+if (!defined('_TB_VERSION_'))
 	exit;
 
 class BlockTags extends Module
@@ -33,8 +33,8 @@ class BlockTags extends Module
 	{
 		$this->name = 'blocktags';
 		$this->tab = 'front_office_features';
-		$this->version = '1.3.1';
-		$this->author = 'PrestaShop';
+		$this->version = '2.0.0';
+		$this->author = 'thirty bees';
 		$this->need_instance = 0;
 
 		$this->bootstrap = true;
