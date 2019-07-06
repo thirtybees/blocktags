4314a14a45268507e7aaf96cbc2106ec43bb8ecf
Petr Hučík <petr@getdatakick.com>
2019-07-06 10:38:54 +0200

    Release version 2.0.2

--- blocktags.php
+++ blocktags.php
@@ -32,7 +32,7 @@ class BlockTags extends Module
 	{
 		$this->name = 'blocktags';
 		$this->tab = 'front_office_features';
-		$this->version = '2.0.1';
+		$this->version = '2.0.2';
 		$this->author = 'thirty bees';
 		$this->need_instance = 0;
 
